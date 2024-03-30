c7fdf9b84e82cd1d1f5ebba7002e434b5bacf068
mmy3834 <mstfyldzitu@gmail.com>
2024-03-30 13:34:24 +0300

    DataTables

---
+++ Frontends/CarBook.Dto/ContactDtos/ResultContactDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Dto.ContactDtos
+{
+    public class ResultContactDto
+    {
+
+        public int contactId { get; set; }
+        public string? name { get; set; }
+        public string? email { get; set; }
+        public string? subject { get; set; }
+        public string? message { get; set; }
+        public DateTime sendDate { get; set; }
+
+
+    }
+}
